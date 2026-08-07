bb3958c1364a3b10784805e378252ee0ee42a3e2
Shivam Singh <ald.shivamsingh@gmail.com>
2026-08-06 23:51:55 -0400

    Add: sql-bank-account-summary-ii - submission-34

---
+++ SQL/sql-bank-account-summary-ii/submission-34.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+-- Write your query below
+SELECT u.name, SUM(t.amount) AS balance
+FROM users u
+JOIN transactions t ON u.account = t.account
+GROUP BY u.account
+HAVING SUM(t.amount) > 10000;
